9c5d71d90659a515a5aa4ec0428e0241d81a63dd
Guilherme de Souza Fernandes <120669409+Gui1213x@users.noreply.github.com>
2023-02-19 00:51:09 -0300

    Update BasicSecurityConfig.java

--- Security Blog/blogpessoal/src/main/java/org/generation/blogpessoal/security/BasicSecurityConfig.java
+++ Security Blog/blogpessoal/src/main/java/org/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -48,3 +48,112 @@ public class BasicSecurityConfig {
     }
 
 }
+
+----------------------------------------
+    
+    package org.generation.blogpessoal.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.generation.blogpessoal.model.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+
+public class UserDetailsImpl implements UserDetails{
+
+private static final long serialVersionUID =1L;
+	
+	private String userName;
+	private String password;
+	
+	private List<GrantedAuthority> authorities;
+	
+	public UserDetailsImpl (Usuario user){
+		this.userName = user.getUsuario();
+		this.password = user.getSenha();
+	}
+	
+	public UserDetailsImpl (){ }
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+	
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+	
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		
+		return userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		
+		return true;
+	}
+
+    @Override
+	public boolean isEnabled() {
+		
+		return true;
+	}
+
+}
+
+--------------------------------------
+    
+    package org.generation.blogpessoal.security;
+
+import java.util.Optional;
+
+import org.generation.blogpessoal.model.Usuario;
+import org.generation.blogpessoal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+		Optional<Usuario> usuario = usuarioRepository.findByUsuario(userName);
+
+		if (usuario.isPresent())
+			return new UserDetailsImpl(usuario.get());
+		else
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+	}
+}
