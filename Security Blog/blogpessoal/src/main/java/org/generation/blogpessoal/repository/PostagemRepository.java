95211031ff0666aced6b3a349b511b3d700c4fb2
Guilherme de Souza Fernandes <120669409+Gui1213x@users.noreply.github.com>
2023-02-19 00:46:47 -0300

    Update PostagemRepository.java

--- Security Blog/blogpessoal/src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
+++ Security Blog/blogpessoal/src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -14,3 +14,34 @@ public interface PostagemRepository extends JpaRepository<Postagem, Long> {
     List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
 }
+
+
+package org.generation.blogpessoal.repository;
+
+import java.util.List;
+
+import org.generation.blogpessoal.model.Tema;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+
+}
+
+
+package org.generation.blogpessoal.repository;
+
+import java.util.Optional;
+
+import org.generation.blogpessoal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	public Optional<Usuario> findByUsuario(String usuario);
+	
+}
