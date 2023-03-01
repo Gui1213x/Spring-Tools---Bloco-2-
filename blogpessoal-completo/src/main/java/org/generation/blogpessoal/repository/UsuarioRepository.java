eb0a1eeef8669d86ce2897e0416138e717f8e85a
Guilherme de Souza Fernandes <120669409+Gui1213x@users.noreply.github.com>
2023-03-01 09:15:30 -0300

    Update UsuarioRepository.java

--- blogpessoal-completo/src/main/java/org/generation/blogpessoal/repository/UsuarioRepository.java
+++ blogpessoal-completo/src/main/java/org/generation/blogpessoal/repository/UsuarioRepository.java
@@ -19,3 +19,23 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	public List <Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);	
 	
 }
+
+---- 
+	
+	
+
+package org.generation.db_blogpessoal.repository;
+
+import java.util.List;
+
+import org.generation.db_blogpessoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface postagemRepository extends JpaRepository<Postagem, Long> {
+
+    List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
+}
