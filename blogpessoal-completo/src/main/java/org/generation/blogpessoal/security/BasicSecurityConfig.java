ce856a18d37ecc8ca95cea79ca2ba8e282d5fdfa
Guilherme de Souza Fernandes <120669409+Gui1213x@users.noreply.github.com>
2023-03-01 09:17:15 -0300

    Update BasicSecurityConfig.java

--- blogpessoal-completo/src/main/java/org/generation/blogpessoal/security/BasicSecurityConfig.java
+++ blogpessoal-completo/src/main/java/org/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -50,3 +50,166 @@ public class BasicSecurityConfig {
     }
 
 }
+
+-----
+    
+    package org.generation.db_blogpessoal.security;
+
+
+import java.util.Collection;
+import java.util.List;
+
+import org.generation.db_blogpessoal.model.Usuario;
+import org.generation.db_blogpessoal.model.UsuarioLogin;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	private String userName;
+	private String password;
+
+	private List<GrantedAuthority> authorities;
+
+	public UserDetailsImpl(Usuario user) {
+		this.userName = user.getUsuario();
+		this.password = user.getSenha();
+	}
+
+	public UserDetailsImpl() {
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+
+		return userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+
+		return true;
+	}
+
+}
+
+--- 
+    
+    package org.generation.db_blogpessoal.security;
+
+	import java.util.Optional;
+
+import org.generation.db_blogpessoal.model.Usuario;
+import org.generation.db_blogpessoal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+	@Service
+	public class UserDetailsServiceImpl implements UserDetailsService {
+		
+		@Autowired
+		private UsuarioRepository usuarioRepository;
+
+		@Override
+		public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+			Optional<Usuario> usuario = usuarioRepository.findByUsuario(userName);
+
+			if (usuario.isPresent())
+				return new UserDetailsImpl(usuario.get());
+			else
+				throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+		}
+	}
+
+
+----- 
+    
+    package org.generation.db_blogpessoal.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class BasicSecurityConfig {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().csrf().disable()
+            .cors();
+
+        http
+            .authorizeHttpRequests((auth) -> auth
+                .antMatchers("/usuarios/logar").permitAll()
+                .antMatchers("/usuarios/cadastrar").permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest().authenticated())
+            .httpBasic();
+
+        return http.build();
+
+    }
+
+}
