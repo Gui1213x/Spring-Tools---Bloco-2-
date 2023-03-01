607a5f0a4335429ad0c7e127c886980e0f32ff04
Guilherme de Souza Fernandes <120669409+Gui1213x@users.noreply.github.com>
2023-03-01 08:40:40 -0300

    Update BasicSecurityConfig.java

--- Security loja game/loja-de-games/src/main/java/org/generation/lojadegames/secutiry/BasicSecurityConfig.java
+++ Security loja game/loja-de-games/src/main/java/org/generation/lojadegames/secutiry/BasicSecurityConfig.java
@@ -51,19 +51,69 @@ public class BasicSecurityConfig {
 
 ---  -   --  
     
-    package org.generation.lojadegames.secutiry;
+    package org.generation.gengames.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class BasicSecurityConfig {
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().csrf().disable()
+            .cors();
+
+        http
+            .authorizeHttpRequests((auth) -> auth
+                .antMatchers("/usuarios/logar").permitAll()
+                .antMatchers("/usuarios/cadastrar").permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest().authenticated())
+            .httpBasic();
+
+        return http.build();
+    }
+}
+
+
+------- --- 
+	
+	package org.generation.gengames.security;
 
 import java.util.Collection;
 import java.util.List;
 
-import org.generation.lojadegames.model.Usuario;
+import org.generation.gengames.model.Usuario;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-
-
-public class UserDetailsImpl implements UserDetails{
-
+public class UserDetailsImpl implements UserDetails {
+	
 private static final long serialVersionUID =1L;
 	
 	private String userName;
@@ -75,8 +125,7 @@ private static final long serialVersionUID =1L;
 		this.userName = user.getUsuario();
 		this.password = user.getSenha();
 	}
-	
-	public UserDetailsImpl (){ }
+public UserDetailsImpl (){ }
 	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -123,24 +172,20 @@ private static final long serialVersionUID =1L;
 }
 
 
----- --- -- --- 
+----- --- 
 	
-	package org.generation.lojadegames.secutiry;
+	package org.generation.gengames.security;
 
 import java.util.Optional;
 
-import org.generation.lojadegames.model.Usuario;
-import org.generation.lojadegames.repository.UsuarioRepository;
+import org.generation.gengames.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-
-@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 	
 	@Autowired
@@ -149,7 +194,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 
-		Optional<Usuario> usuario = usuarioRepository.findByUsuario(userName);
+		Optional<org.generation.gengames.model.Usuario> usuario = usuarioRepository.findByUsuario(userName);
 
 		if (usuario.isPresent())
 			return new UserDetailsImpl(usuario.get());
@@ -157,3 +202,4 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 			throw new ResponseStatusException(HttpStatus.FORBIDDEN);
 	}
 }
+
